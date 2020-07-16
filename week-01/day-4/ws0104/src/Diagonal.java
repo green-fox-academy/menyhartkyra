312137d26224d078d2bd988e7b6237ecb84f2d19
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-16 15:36:22 +0200

    Diagonal

---
+++ week-01/day-4/ws0104/src/Diagonal.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Diagonal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int Lines = scanner.nextInt();
+
+        for (int j = 0; j < Lines; j++) {
+            System.out.print("%");
+        }
+        System.out.println("");
+
+        for (int i = 1; i < Lines-1 ; i++) {
+            System.out.print("%");          //%jel
+            for (int k = 0; k < i-1 ; k++) { //spacek elöl
+                System.out.print(" ");
+            }
+            System.out.print("%");      ////%jel
+            for (int k = 0; k < Lines-2-i ; k++) { //spacek hátul
+                System.out.print(" ");
+            }
+            System.out.print("%\n");    //%jel
+        }
+
+        for (int j = 0; j < Lines; j++) {
+            System.out.print("%");
+        }
+        System.out.println("");
+
+        scanner.close();
+    }
+}
