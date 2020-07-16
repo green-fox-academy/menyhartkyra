167c438f28bcb15512999bd8725e798a3a12473a
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-16 13:53:55 +0200

    exercise CountFromTo

---
+++ week-01/day-4/ws0104/src/CountFromTo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CountFromTo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int FirstNumber= scan.nextInt();
+        int SecondNumber = scan.nextInt();
+
+        if (SecondNumber <= FirstNumber){
+            System.out.println("Second number should be bigger");
+        }else{
+            int Diff = SecondNumber - FirstNumber;
+            for (int i=0; i<Diff; ++i){
+                System.out.println(FirstNumber+i);
+            }
+        }
+
+        scan.close();
+    }
+}
