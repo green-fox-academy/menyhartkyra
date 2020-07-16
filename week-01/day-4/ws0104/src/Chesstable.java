7106e3b6b042435503b05325633faaaba1a6bf1c
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-16 16:08:34 +0200

    Chesstable

---
+++ week-01/day-4/ws0104/src/Chesstable.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Chesstable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int Lines = scanner.nextInt();
+
+        for (int i = 0; i < Lines; i++) {
+            if ( i %2 == 0 ){
+                for (int j = 0; j < Lines/2 ; j++) {
+                    System.out.print("% ");
+                }
+                System.out.println("");
+            }else{
+                for (int j = 0; j < Lines/2 ; j++) {
+                    System.out.print(" %");
+                }
+                System.out.println("");
+            }
+        }
+        scanner.close();
+    }
+}
