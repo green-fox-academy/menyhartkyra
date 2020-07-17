6b3bfad7959c1fa7fb1eb4700d143542d1176682
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-17 16:12:43 +0200

    substring

--- week-01/day-4/ws0104/src/substr2.java
+++ week-01/day-4/ws0104/src/substr2.java
@@ -3,18 +3,16 @@ public class substr2 {
   public static void main(String[] args) {
     //System.out.println(q.length());
     //System.out.println(q.charAt(0));
-    System.out.println(substrk("this is what I'm in", "searching"));
+    System.out.println(substrk("this is what searching I'm in", "searching"));
   }
 
   public static int substrk(String input, String q) {
     int result = -1;
-    int i;
-    int k = 0;
 
     outer:
-    for (i = 0; i <= input.length() - q.length(); i++) {
+    for (int i = 0; i <= input.length() - q.length(); i++) {
 
-      for (k = 0; k < q.length(); k++) {
+      for (int k = 0; k < q.length(); k++) {
         if (input.charAt(i + k) == q.charAt(k)) {
           if (k == q.length() - 1) {    //szó végére értünk
             result = i;
