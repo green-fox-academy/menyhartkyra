98c2414918c5d84b619e283971575f738804e727
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-20 14:56:52 +0200

    Sum.java

---
+++ week-02/day-1/src/Sum.java
@@ -0,0 +1,14 @@
+public class Sum {
+  public static void main(String[] args) {
+    // Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+    int number = 6;
+    System.out.println(sum(number));
+  }
+
+  public static int sum(int number) {
+    int sum = 0;
+    for (int i = 0; i < number + 1; i++) 
+      sum += i;
+    return sum;
+  }
+}
