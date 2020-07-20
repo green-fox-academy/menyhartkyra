15bbcc935378d5ffe23d977121a0077f213a9e2b
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-20 15:57:24 +0200

    Subint.java

---
+++ week-02/day-1/src/SubInt.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class SubInt {
+  public static void main(String[] args) {
+    //  Create a function that takes a number and an array of integers as a parameter
+    //  Returns the indices of the integers in the array of which the first number is a part of
+    //  Or returns an empty array if the number is not part of any of the integers in the array
+
+    //  Example:
+    System.out.println(subInt(1, new int[] {1, 11, 34, 52, 61}));
+    //  should print: `[0, 1, 4]`
+    System.out.println(subInt(9, new int[] {1, 11, 34, 52, 61}));
+    //  should print: '[]'
+  }
+
+  public static String subInt(int number, int[] array) {
+    String strNum = Integer.toString(number);
+    //System.out.println("strNum: " + strNum);
+    String[] strArray = new String[array.length];
+    int resultLength = 0;
+    int[] preresult = new int[array.length];
+    for (int i = 0; i < array.length; i++) {
+      strArray[i] = Integer.toString(array[i]);
+      if (strArray[i].contains(strNum)) {
+        preresult[i] = i;
+        resultLength += 1;
+      } else {
+        preresult[i] = 0;
+      }
+    }
+    //System.out.println(Arrays.toString(strArray));
+    //System.out.println("preresult: " + Arrays.toString(preresult));
+    int[] result = new int[resultLength];
+    int cnt = 0;
+    for (int j = 0; j < preresult.length; j++) {
+      if (preresult[j] != 0) {
+        result[cnt] = preresult[j];
+        cnt++;
+      }
+    }
+    Arrays.sort(result);
+    return Arrays.toString(result);
+  }
+
+}
