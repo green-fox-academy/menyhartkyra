6f0b1a48e3b6b11067741e96c27305143e183c92
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-20 16:34:12 +0200

    Unique.java

---
+++ week-02/day-1/src/Unique.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class Unique {
+  public static void main(String[] args) {
+    //  Create a function that takes a list of numbers as a parameter
+    //  Returns a list of numbers where every number in the list occurs only once
+
+    //  Example
+    System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+    //  should print: `[1, 11, 34, 52, 61]`
+  }
+
+  public static List<Integer> unique(int[] numbers) {
+    Integer[] numbersIntiger = IntStream.of(numbers).boxed().toArray(Integer[]::new);
+    Set<Integer> set = new HashSet<Integer>(Arrays.asList(numbersIntiger));
+    List<Integer> sortedList = new ArrayList<>(set);
+    Collections.sort(sortedList);
+    return sortedList;
+  }
+}
