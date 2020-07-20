c6ae3d6984494801e9e426cd069531d4a9e9fd93
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-20 14:27:16 +0200

    SwapElements.java

---
+++ week-02/day-1/src/SwapElements.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class SwapElements {
+  public static void main(String[] args) {
+    // - Create an array variable named `orders`
+    //   with the following content: `["first", "second", "third"]`
+    // - Swap the first and the third element of `orders`
+    String[] orders = new String[] {"first", "second", "third"};
+    String temp = orders[0];
+    orders[0] = orders[2];
+    orders[2] = temp;
+    System.out.println(Arrays.toString(orders));
+  }
+}
