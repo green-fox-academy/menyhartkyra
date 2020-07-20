baa339376361a13f38f97ad8c4fb2440703215b3
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-20 14:18:33 +0200

    Colors.java

---
+++ week-02/day-1/src/Colours.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Colours {
+  public static void main(String[] args) {
+    // - Create a two dimensional array
+    //   which can contain the different shades of specified colors
+    // - In `colors[0]` store the shades of green:
+    //   `"lime", "forest green", "olive", "pale green", "spring green"`
+    // - In `colors[1]` store the shades of red:
+    //   `"orange red", "red", "tomato"`
+    // - In `colors[2]` store the shades of pink:
+    //   `"orchid", "violet", "pink", "hot pink"`
+    String[][] colors = new String[3][];
+    colors[0] = new String[] {"lime", "forest green", "olive", "pale green", "spring green"};
+    colors[1] = new String[] {"orange red", "red", "tomato"};
+    colors[2] = new String[] {"orchid", "violet", "pink", "hot pink"};
+
+    for (String[] rows: colors) {
+      System.out.println(Arrays.toString(rows));
+    }
+
+  }
+}
