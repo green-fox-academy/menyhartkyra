6f832951b16a03f90c372f32bd132e28f262d9c5
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-20 13:58:56 +0200

    Matrix

---
+++ week-02/day-1/src/Matrix.java
@@ -0,0 +1,25 @@
+public class Matrix {
+  public static void main(String[] args) {
+    // - Create (dynamically) a two dimensional array
+    //   with the following matrix. Use a loop!
+    //
+    //   1 0 0 0
+    //   0 1 0 0
+    //   0 0 1 0
+    //   0 0 0 1
+    //
+    // - Print this two dimensional array to the output
+    int[][] matrix = new int[4][4];
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        if (i == j)
+          matrix[i][j] = 1;
+        else
+          matrix[i][j] = 0;
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+
+  }
+}
