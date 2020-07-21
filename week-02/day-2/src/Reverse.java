25fa4b2e19bb44ac7f41e957999b3dc43136ce62
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-21 12:00:53 +0200

    Reverse.java

---
+++ week-02/day-2/src/Reverse.java
@@ -0,0 +1,19 @@
+public class Reverse {
+  public static void main(String... args){
+    String toBeReversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+    // Create a method that can reverse a String, which is passed as the parameter
+    // Use it on this reversed string to check it!
+    // Try to solve this using charAt() first, and optionally anything else after.
+
+    System.out.println(reverse(toBeReversed));
+  }
+
+  public static String reverse(String toBeReversed){
+    String Reversed="";
+    for (int i = 0; i < toBeReversed.length(); i++) {
+      Reversed += toBeReversed.charAt(toBeReversed.length()-1-i);
+    }
+    return Reversed;
+  }
+}
