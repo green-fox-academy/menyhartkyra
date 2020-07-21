03e0d756da9d39c0f1e4ba3876b5162cf676d747
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-21 13:24:17 +0200

    ListIntroduction1

---
+++ week-02/day-2/src/ListIntroduction1.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ListIntroduction1 {
+  public static void main(String[] args) {
+    ArrayList<String> names = new ArrayList<>();
+    System.out.println(names.size());
+    names.add("William");
+    System.out.println(names.isEmpty());
+    names.add("John");
+    names.add("Amanda");
+    System.out.println(names.size());
+    System.out.println(names.get(2));
+    for ( String name : names ) {
+      System.out.println(names.indexOf(name)+1 + ". "  + name);
+    }
+    Iterator<String> name = names.iterator();
+    System.out.println("Iterator:");
+    while(name.hasNext()){
+      System.out.println(name.next());
+    }
+    names.remove(1);
+    System.out.println("Reversed:");
+    for (int i = names.size()-1; i >= 0 ; i--) {
+      System.out.println(names.get(i));
+    }
+    names.clear();
+    System.out.println(names);
+
+  }
+}
