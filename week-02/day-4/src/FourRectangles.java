c36235120863917e4918ae58fe36cb8ccb1b6d6a
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-23 13:34:44 +0200

    added FourRectangles.java

---
+++ week-02/day-4/src/FourRectangles.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FourRectangles {
+
+  public static void mainDraw(Graphics graphics){
+    // draw four different size and color rectangles.
+    // avoid code duplication.
+    for (int i = 1; i < 5; i++) {
+      graphics.setColor(getRandomColor());
+      graphics.drawRect(i*10*i, 10, i*10, i*10);
+    }
+
+
+  }
+
+  public static Color getRandomColor() {
+    Random random = new Random();
+    int limit = 256;
+    return new Color(random.nextInt(limit), random.nextInt(limit), random.nextInt(limit));
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
