e592b5107f7b28efa2381542255c7731246c1d18
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-28 10:19:03 +0200

    teacherstudent

---
+++ week-03/day-2/src/teacherstudent/Main.java
@@ -0,0 +1,11 @@
+package teacherstudent;
+
+public class Main {
+  public static void main(String[] args) {
+    Teacher teacher = new Teacher();
+    Student student = new Student();
+
+    student.question(teacher);
+    teacher.teach(student);
+  }
+}
---
+++ week-03/day-2/src/teacherstudent/Student.java
@@ -0,0 +1,13 @@
+package teacherstudent;
+
+public class Student {
+
+  public void learn(){
+    System.out.println("the student is learning sth new");
+  }
+
+  public void question(Teacher teacher){
+    teacher.answer();
+  }
+
+}
