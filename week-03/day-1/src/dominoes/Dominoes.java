7fe9d1479ee4d008e84f12176bed70a1fd68f4db
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-28 09:45:42 +0200

    dominoes

--- week-03/day-1/src/dominoes/Dominoes.java
+++ week-03/day-1/src/dominoes/Dominoes.java
@@ -1,4 +1,5 @@
 package dominoes;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,28 +11,37 @@ public class Dominoes {
     // eg: [2, 4], [4, 3], [3, 5] ...
     System.out.println(dominoes);
 
-    List<Domino> dominosOrdered = new ArrayList<>();
-    int listLenght = dominoes.size();
-    dominosOrdered.add(dominoes.get(0));
+    int size = dominoes.size();
+    outer:
+    for ( int k =0; k < size; k++ ) {       //elore viszi ha van olyan szam bal oldalon, aminek nincs parja
+      int left = dominoes.get(k).getLeftSide();
+      int i;
+      for (i = 0; i < size; i++) {
+        if (left == dominoes.get(i).getRightSide()) {
+          continue outer;
+        }
+      }
+      dominoes.add(0, dominoes.get(k));
+      dominoes.remove(k + 1);
+    }
 
-    for ( int i = 0; i < dominoes.size()-1; i++ ) {
-      int k = i+1;
-      while (dominoes.get(i).getRightSide() != dominoes.get(k).getLeftSide()){
+    for (int i = 0; i < dominoes.size() - 1; i++) {
+      int k = i + 1;
+      while (dominoes.get(i).getRightSide() != dominoes.get(k).getLeftSide()) {
         k++;
       }
-      dominoes.add(i+1,dominoes.get(k));
-      dominoes.remove(k+1);
+      dominoes.add(i + 1, dominoes.get(k));
+      dominoes.remove(k + 1);
     }
     System.out.println(dominoes);
   }
-
 
 
   static List<Domino> initializeDominoes() {
     List<Domino> dominoes = new ArrayList<>();
     dominoes.add(new Domino(5, 2));
     dominoes.add(new Domino(4, 6));
-    dominoes.add(new Domino(1, 5));
+    dominoes.add(new Domino(9, 5));
     dominoes.add(new Domino(6, 7));
     dominoes.add(new Domino(2, 4));
     dominoes.add(new Domino(7, 1));
