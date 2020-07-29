b7266e49230bc5c9ddc03aaa4814ee685bc3b132
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-29 13:50:38 +0200

    fibonacciLong

--- week-03/day-3/src/fibonacci/Fibonacci.java
+++ week-03/day-3/src/fibonacci/Fibonacci.java
@@ -3,13 +3,13 @@ package fibonacci;
 public class Fibonacci {
   public static void main(String[] args) {
     try {
-      System.out.println(fibonacci(4));
+      System.out.println(fibonacci(50));
     } catch (StackOverflowError e) {
       System.out.println("don't give me a negative number");
     }
   }
 
-  public static int fibonacci(int nth) {
+  public static long fibonacci(long nth) {
     if (nth == 0 || nth == 1) {
       return 1;
     } else {
