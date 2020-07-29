f8e97de95880ecb296a1515d4b53bde97b35f898
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-29 13:22:22 +0200

    stringsAgain

--- week-03/day-3/src/strings/StringsAgain.java
+++ week-03/day-3/src/strings/StringsAgain.java
@@ -1,22 +1,22 @@
 package strings;
 
-public class Strings {
+public class StringsAgain {
   public static void main(String[] args) {
     try {
-      System.out.println(changeXtoY(""));
+      System.out.println(removeX("valaximixi"));
     } catch (java.lang.StringIndexOutOfBoundsException e) {
       System.out.println("this is not enough string");
     }
   }
 
-  public static String changeXtoY(String s) {
+  public static String removeX(String s) {
     if (s.length() == 1) {
       return s;
     } else {
       if (s.charAt(0) == 'x') {
-        return 'y' + changeXtoY(s.substring(1));
+        return removeX(s.substring(1));
       } else {
-        return s.charAt(0) + changeXtoY(s.substring(1));
+        return s.charAt(0) + removeX(s.substring(1));
       }
     }
   }
