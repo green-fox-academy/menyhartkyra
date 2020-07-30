96e8be163605d0d9ed427b67c1b527a2a15f0643
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-07-30 17:33:13 +0200

    changes in pirate, sharpieset...

---
+++ week-03/day-4/src/test/ExtensionTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+class ExtensionTest {
+
+  @Test
+  void add() {
+  }
+
+  @Test
+  void maxOfThree() {
+  }
+
+  @Test
+  void median() {
+  }
+
+  @Test
+  void isVowel() {
+  }
+
+  @Test
+  void translate() {
+  }
+}
---
+++ week-03/day-4/src/test/FibonacciTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+class FibonacciTest {
+
+  @Test
+  void fibonacci() {
+  }
+}
