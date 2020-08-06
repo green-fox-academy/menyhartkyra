3b52b686df7f283eba93f02ebed23a0cc26f0991
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-08-06 15:37:22 +0200

    corrections made during code review

---
+++ week-04/day-4/reservations/Reservations.java
@@ -0,0 +1,15 @@
+package reservations;
+
+public class Reservations implements Reservationy {
+  String[] DOW
+
+  @Override
+  public String getDowBooking() {
+    return null;
+  }
+
+  @Override
+  public String getCodeBooking() {
+    return null;
+  }
+}
