1411c72995c5a4d2af0f792294160fbc3716ecbf
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-08-06 16:04:58 +0200

    'reservations' task done

--- week-04/day-4/reservations/Main.java
+++ week-04/day-4/reservations/Main.java
@@ -1,4 +1,10 @@
 package reservations;
 
 public class Main {
+  public static void main(String[] args) {
+    for (int i = 0; i < 8; i++) {
+      Reservations reservation = new Reservations();
+      System.out.printf("Booking# %s for %s%n", reservation.code, reservation.DoW);
+    }
+  }
 }
--- week-04/day-4/reservations/Reservations.java
+++ week-04/day-4/reservations/Reservations.java
@@ -1,15 +1,43 @@
 package reservations;
 
+import java.util.Random;
+
 public class Reservations implements Reservationy {
-  String[] DOW
+  static String[] DOW = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+  String DoW;
+  String code;
+
+  public Reservations(){
+    DoW = getDowBooking();
+    code = getCodeBooking();
+  }
 
   @Override
   public String getDowBooking() {
-    return null;
+    return DOW[getRandomNumber(0, 6)];
+  }
+
+  int getRandomNumber(int lowerLimit, int upperLimit) {
+    int interval = upperLimit - lowerLimit;
+    return (int) (lowerLimit + Math.random() * interval);
   }
 
   @Override
   public String getCodeBooking() {
-    return null;
+    String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+    Random random = new Random();
+    String code = "";
+    for (int i = 0; i < 8; i++) {
+      code = code.concat(String.valueOf(characters.charAt(random.nextInt(34))));
+    }
+    return code;
+  }
+
+  @Override
+  public String toString() {
+    return "Reservations{" +
+        "DoW='" + DoW + '\'' +
+        ", code='" + code + '\'' +
+        '}';
   }
 }
