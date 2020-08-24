855a0216de83f5545a0e848cd79e403157402fd9
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-08-24 17:04:59 +0200

    exercise 4 done

--- week-06/day-1/Main.java
+++ week-06/day-1/Main.java
@@ -8,12 +8,20 @@ public class Main {
 
   public static void main(String[] args) {
     List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
-
     EvenNumbers evenNumbers = new EvenNumbers();
     evenNumbers.printEvenNumbers(numbers);
 
+    //exercise 2
     SquaredValueOfPositiveNumbers.printSqaredValueOfPositiveNumbers(numbers);
 
+    //exercise 4
+    double averageOfOdds =
+        numbers.stream()
+            .filter(n -> n % 2 != 0)
+            .collect(Collectors.averagingInt(n -> n));
+    System.out.println(averageOfOdds);
+
+    //exercise 3
     List<Integer> numbers3 = Arrays.asList(3, 9, 2, 8, 6, 5);
 //    Map<Integer, Integer> map = numbers3.stream()
 //        .collect(Collectors.toMap(
