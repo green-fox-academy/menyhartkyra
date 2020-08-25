ae6b21e164b5b468587dfed9465e35abb606e7b2
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-08-25 17:29:06 +0200

    worked with predicate functional interface

--- week-06/day-1/EvenNumbers.java
+++ week-06/day-1/EvenNumbers.java
@@ -1,13 +1,24 @@
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class EvenNumbers {
+public class EvenNumbers implements Predicate<Integer> {
 
   public void printEvenNumbers(List<Integer> numbers){
     List<Integer> evenNumbers = numbers.stream()
         .filter(number -> number % 2 == 0)
         .collect(Collectors.toList());
     System.out.println(evenNumbers);
+
+    System.out.println("with own predicate");
+    List<Integer> evenNumbers2 = numbers.stream()
+        .filter(this::test)
+        .collect(Collectors.toList());
+    System.out.println(evenNumbers);
   }
 
+  @Override
+  public boolean test(Integer number) {
+    return number %2 == 0;
+  }
 }
