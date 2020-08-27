e133d86d4fb1d64bc888beee40d314eeb23baa39
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-08-27 17:09:30 +0200

    search bar added, but not searching yet

--- week-06/day-3/myshop/src/main/java/kyra/myshop/controllers/Controller.java
+++ week-06/day-3/myshop/src/main/java/kyra/myshop/controllers/Controller.java
@@ -1,6 +1,10 @@
 package kyra.myshop.controllers;
 
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -8,5 +12,10 @@ public class Controller {
   @RequestMapping("")
   public String showItems(){
     return "shop";
+  }
+
+  @RequestMapping(value = "/search", method = POST)
+  public String search(){
+    return "redirect:";
   }
 }
