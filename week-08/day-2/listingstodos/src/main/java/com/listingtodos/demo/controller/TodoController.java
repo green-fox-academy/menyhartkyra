2950ea72ca7d426a7050e13b22ad4d277e8ff44b
Menyhárt Kyra <menyhartkyra@gmail.com>
2020-09-09 17:22:49 +0200

    added search bar to tasks

--- week-08/day-2/listingstodos/src/main/java/com/listingtodos/demo/controller/TodoController.java
+++ week-08/day-2/listingstodos/src/main/java/com/listingtodos/demo/controller/TodoController.java
@@ -2,17 +2,12 @@ package com.listingtodos.demo.controller;
 
 import com.listingtodos.demo.model.Todo;
 import com.listingtodos.demo.repository.TaskRepository;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TodoController {
@@ -23,20 +18,15 @@ public class TodoController {
     this.taskRepository = taskRepository;
   }
 
-//  @RequestMapping(path = "/todo")
-//  public String renderMainPage(){
-//    return "todolist";
-//  }
 
   @RequestMapping(path = {"/", "/list"})
   public String list(Model model, @RequestParam(required = false) String isActive) {
     if (isActive == null) {
       model.addAttribute("todos", taskRepository.findAll());
-    } else {
-     List<Todo> tasks = new ArrayList<>();
-     taskRepository.findAll().forEach(tasks::add);
-     tasks = tasks.stream().filter(x -> !x.isDone()).collect(Collectors.toList());
-      model.addAttribute("todos", tasks);
+    } else if (isActive.equals("true")) {
+      model.addAttribute("todos", taskRepository.findAllByDone(false));
+    } else if (isActive.equals("false")) {
+      model.addAttribute("todos", taskRepository.findAllByDone(true));
     }
     return "todolist";
   }
@@ -48,17 +38,22 @@ public class TodoController {
   }
 
   @RequestMapping(value = "/add-new-task", method = RequestMethod.POST)
-  public String addNewTask(String newTaskName, @RequestParam(required = false) String isTaskUrgent,
-                           @RequestParam(required = false) String isTaskDone) {
-    boolean urgent = isTaskUrgent != null;
-    boolean done = isTaskDone != null;
-    taskRepository.save(new Todo(newTaskName, urgent, done));
+  public String addNewTask(String newTaskName, @RequestParam(required = false) boolean
+      isTaskUrgent, @RequestParam(required = false) boolean isTaskDone) {
+
+    taskRepository.save(new Todo(newTaskName, isTaskUrgent, isTaskDone));
     return "redirect:/";
   }
 
   @RequestMapping(value = "/remove", method = RequestMethod.POST)
-  public String removeTask(long taskIndexToRemove){
+  public String removeTask(long taskIndexToRemove) {
     taskRepository.deleteById(taskIndexToRemove);
     return "redirect:/";
+  }
+
+  @RequestMapping(path = "/find", method = RequestMethod.POST)
+  public String findTask(String taskToFind, Model model){
+    model.addAttribute("todos",taskRepository.findByTitleContaining(taskToFind));
+    return "todolist";
   }
 }
--- week-08/day-2/listingstodos/src/main/java/com/listingtodos/demo/repository/TaskRepository.java
+++ week-08/day-2/listingstodos/src/main/java/com/listingtodos/demo/repository/TaskRepository.java
@@ -1,10 +1,12 @@
 package com.listingtodos.demo.repository;
 
 import com.listingtodos.demo.model.Todo;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends CrudRepository<Todo, Long> {
-
+public List<Todo> findAllByDone(boolean done);
+public List<Todo> findByTitleContaining(String title);
 }
